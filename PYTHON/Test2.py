d396a05e870e23266e45b5db37e86a1c2e916565
W9JAK <112979587+W9JAK@users.noreply.github.com>
2025-01-04 20:41:39 +0300

    доделал калькулятор

--- PYTHON/Test2.py
+++ PYTHON/Test2.py
@@ -1,37 +1,54 @@
 #Stupid calculater 
 
-what=input("Что делаем? (+;-;*;/;округление):")
-a=float(input("введи первое число:"))
-b=float(input("Введи второе число:"))
-
-
+what=input("Что делаем? (+;-;*;/;round;%):")
 
 # я думал import math поможет, но нет. Он не может уместить сразу много задач в себе. На возведении в степень и проценте пошел сбой.. 
 
 
+if what in {"+", "-", "*", "/", "**"}:
 
-if what=="+":
-    c=a+b
-    print("результат:"+str(c))
+    a=float(input("введи первое число:"))
+    b=float(input("Введи второе число:"))
 
-if what=="-": 
-    c=a-b
-    print("результат:"+str(c))
+    if what=="+":
+        c=a+b
+        print("результат:"+str(c))
 
-if what=="*":
-    c=a*b
-    print("результат:"+str(c))
+    elif what=="-": 
+        c=a-b
+        print("результат:"+str(c))
 
-if what=="/":
-    c=a/b
-    print("хуй:"+str(c))
+    elif what=="*":
+        c=a*b
+        print("результат:"+str(c))
 
-elif what=="**":
-    c=a**
-    print("результат:"+str(c))
+    elif what=="/":
+        c=a/b
+        print("хуй:"+str(c))
 
+    elif what=="**":
+        c=a**b
+        print("результат:"+str(c))
+
+    else:
+        print("Неверные вводные")
+
+elif what in {"round", "%"}:
+
+    a=float(input("введи число:"))
+
+    if what=="round":
+        print("результат:"+str(round(a)))
+
+    elif what=="%":
+        b=float(input("введи процент:"))
+        c=round(a/100*b)
+        print(f"Процент {b}% от числа {a} будет {c}")
+        
+    else:
+        print("Неверные вводные")
 
 else:
-    print("Неверная комбинация")
+    print("Неверная операция")
 
 
